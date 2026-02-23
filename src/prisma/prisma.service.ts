b4a3242ab0880c0fb651bb0ca2a69eb31f87115c
Trương Hữu Thái <truonghuuthai10022004@gmail.com>
2026-02-23 18:01:07 +0700

    tạo dự án

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService implements OnModuleInit, OnModuleDestroy {
+  private prismaClient: PrismaClient;
+
+  constructor() {
+    const adapter = new PrismaPg({
+      url: process.env.DATABASE_URL,
+    });
+    
+    this.prismaClient = new PrismaClient({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.prismaClient.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.prismaClient.$disconnect();
+  }
+
+  getClient() {
+    return this.prismaClient;
+  }
+}
